844802bc378268b3157ac993b0b02cbf3c8141eb
Linus <linus.gamborn@elev.ga.ntig.se>
2023-01-20 10:01:37 +0100

    Added Router to project

--- Opera Bot/src/main.tsx
+++ Opera Bot/src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './router';
 import App from './App'
 import './index.css'
+import ErrorPage from './components/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
