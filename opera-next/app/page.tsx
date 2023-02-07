0c276f3f9094685ad322337663ae93a2352c526c
Linus <linus.gamborn@elev.ga.ntig.se>
2023-02-07 15:21:05 +0100

    Updated the Home page Content & CSS

--- opera-next/app/page.tsx
+++ opera-next/app/page.tsx
@@ -1,9 +1,30 @@
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="flex flex-col">
+      <div className="flex place-content-center py-5 bg-indigo-700">
+        <h1 className="font-bold text-center">Apex Rollouts</h1>
+      </div>
+
+      <div className="flex flex-row justify-center my-6">
+        <div className="m-4">
+          <p className="max-w-xs p-3 rounded-lg bg-gray-800">Apex Rollouts is the right place if you want to improve your movement skills in Apex Legends.</p>
+        </div>
+        <div className="m-4">
+          <iframe
+            className="rounded-lg"
+            width="426" height="240" 
+            src="https://www.youtube.com/embed/OA4hkmX89ro" 
+            title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            allowFullScreen>
+          </iframe>
+        </div>
+      </div>
+
+      <div>
+
+      </div>
+    </div>
   )
 }
