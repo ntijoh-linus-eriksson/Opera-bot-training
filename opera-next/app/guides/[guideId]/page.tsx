4ab0453eb87bc198144d2c3ba4c68db87e151923
Linus <linus.gamborn@elev.ga.ntig.se>
2023-02-07 15:22:14 +0100

    Updated the Guides page Content & CSS

--- opera-next/app/guides/[guideId]/page.tsx
+++ opera-next/app/guides/[guideId]/page.tsx
@@ -5,11 +5,39 @@ type PageProps = {
 }
 
 function GuidePage({ params: { guideId }}: PageProps) {
-    return (
-        <div>
-            Guide Id: {guideId}
-        </div>
-    );
+    var guideNum: number = +guideId
+
+    switch(guideNum) {
+        case 1: {
+            return (
+                <div className="flex flex-col">
+                    <div className="flex place-content-center my-6">
+                        <h1 className="font-bold text-center">Slide Hopping</h1>
+                    </div>
+                </div>
+            );
+        }
+        case 2: {
+            return (
+                <div className="flex flex-col">
+                    <div className="flex place-content-center my-6">
+                        <h1 className="font-bold text-center">Bunny Hopping</h1>
+                    </div>
+                </div>
+            );
+        }
+        case 3: {
+            return (
+                <div className="flex flex-col">
+                    <div className="flex place-content-center my-6">
+                        <h1 className="font-bold text-center">Wall Hopping</h1>
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    
 }
 
 export default GuidePage
--- opera-next/app/guides/page.tsx
+++ opera-next/app/guides/page.tsx
@@ -3,13 +3,40 @@ import Link from "next/link";
 function Guides() {
     return (
         <div className="flex flex-col">
-            <div className="flex place-content-center my-6">
-                <h1 className="font-bold text-center">MOVEMENT GUIDES</h1>
+            <div className="flex place-content-center py-5 bg-sky-700">
+                <h1 className="font-bold text-center">Movement Guide</h1>
             </div>
-            <div className="flex">
-                <ul>
-                    <li>
-                        <Link href="/guides/1">Wall Bouncing</Link>
+            <div className="flex place-content-evenly my-6">
+                <ul className="flex flex-row flex-wrap max-w-5xl">
+                    <li className="flex flex-col mx-5 my-2">
+                        <h1 className="place-self-center">Slide Hopping</h1>
+                        <img className="my-4" src="https://thumbs.gfycat.com/BarrenUntidyIberianlynx-size_restricted.gif" alt="wall bounce" width="300" height="200" />
+                        <Link className="place-self-center w-48" href="/guides/1"><button className="rounded-full bg-blue-500 w-full px-4 py-2">Learn</button></Link>
+                    </li>
+                    <li className="flex flex-col mx-5 my-2">
+                        <h1 className="place-self-center">Bunny Hopping</h1>
+                        <img className="my-4" src="https://thumbs.gfycat.com/BarrenUntidyIberianlynx-size_restricted.gif" alt="wall bounce" width="300" height="200" />
+                        <Link className="place-self-center w-48" href="/guides/2"><button className="rounded-full bg-blue-500 w-full px-4 py-2">Learn</button></Link>
+                    </li>
+                    <li className="flex flex-col mx-5 my-2">
+                        <h1 className="place-self-center">Wall Hopping</h1>
+                        <img className="my-4" src="https://media.giphy.com/media/UvEh2ckTWWUQ5o4iiA/giphy.gif" alt="wall bounce" width="300" height="200" />
+                        <Link className="place-self-center w-48" href="/guides/3"><button className="rounded-full bg-blue-500 w-full px-4 py-2">Learn</button></Link>
+                    </li>
+                    <li className="flex flex-col mx-5 my-2">
+                        <h1 className="place-self-center"></h1>
+                        <img className="my-4" src="https://media.giphy.com/media/UvEh2ckTWWUQ5o4iiA/giphy.gif" alt="wall bounce" width="300" height="200" />
+                        <Link className="place-self-center w-48" href="/guides/3"><button className="rounded-full bg-blue-500 w-full px-4 py-2">Learn</button></Link>
+                    </li>
+                    <li className="flex flex-col mx-5 my-2">
+                        <h1 className="place-self-center"></h1>
+                        <img className="my-4" src="https://media.giphy.com/media/UvEh2ckTWWUQ5o4iiA/giphy.gif" alt="wall bounce" width="300" height="200" />
+                        <Link className="place-self-center w-48" href="/guides/3"><button className="rounded-full bg-blue-500 w-full px-4 py-2">Learn</button></Link>
+                    </li>
+                    <li className="flex flex-col mx-5 my-2">
+                        <h1 className="place-self-center"></h1>
+                        <img className="my-4" src="https://media.giphy.com/media/UvEh2ckTWWUQ5o4iiA/giphy.gif" alt="wall bounce" width="300" height="200" />
+                        <Link className="place-self-center w-48" href="/guides/3"><button className="rounded-full bg-blue-500 w-full px-4 py-2">Learn</button></Link>
                     </li>
                 </ul>
             </div>
