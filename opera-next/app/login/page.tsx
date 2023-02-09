e099de74a5508c84f028a0deea5ce010da7a501c
Linusriksson <linus.eriksson6@elev.ga.ntig.se>
2023-02-09 13:48:54 +0100

    first stage

--- opera-next/app/login/page.tsx
+++ opera-next/app/login/page.tsx
@@ -1,20 +1,35 @@
 "use client"
 
 import React, { useState } from 'react'
+import { Prisma } from '@prisma/client'
+import { fetcher } from "../../utils/fetcher"
+import prisma from "../../lib/prisma"
 
+export async function getProps() {
+  const users: Prisma.UserUncheckedCreateInput[] = await prisma.user.findMany()
+  return {
+    props: { initialUsers : users }
+  }
+}
 
-const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const submitForm = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-        console.log( email, password);
-    }
+const Login = ({ initialUsers } : any ) => {
+
+  const [users, setUsers] = useState<Prisma.UserUncheckedCreateInput[]>(initialUsers)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
   
     return (
       <div className="container">
-        <form onSubmit={submitForm}>
+        <form 
+          onSubmit={async (e) => {
+            e.preventDefault()
+            const body: Prisma.UserCreateInput = { email, password }
+            await fetcher("/api/user/login", { user : body})
+            console.log(body)
+            setUsers([...users, body])
+            console.log(users)
+          }}
+        >
           <input
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -22,6 +37,7 @@ const Login = () => {
             placeholder="Email"
             className="input"
           />
+          <br />
           <input
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -29,6 +45,7 @@ const Login = () => {
             placeholder="Password"
             className="input"
           />
+          <br />
           <button type="submit" className="btn">Submit</button>
         </form>
       </div>
@@ -36,3 +53,4 @@ const Login = () => {
   };
 
   export default Login;
+  
